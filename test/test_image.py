d11e488279929e48113b90a39517e42fdcb267ee
SimonPBe <muzu22@gmail.com>
2024-01-29 22:29:00 +0100

    fixed testing import and added test

--- test/test_image.py
+++ test/test_image.py
@@ -1,6 +1,16 @@
-
+import sys
+sys.path.append("C:/Users/muzu2/Desktop/OpenSourceS/CelestialCanvas")
 from CelestialCanvas.image import Image
 import numpy as np
+
+
+def test_reset():
+    random_img_data = np.random.rand(10,10)
+    rand_img = Image(random_img_data)
+    rand_img.rescaled_data = np.random.rand(10,10)
+
+    rand_img.reset()
+    assert np.all(rand_img.rescaled_data == random_img_data)
 
 def test_image_normalize():
     random_img_data = np.random.rand(10,10)#random something
@@ -8,3 +18,6 @@ def test_image_normalize():
     norm_data = rand_img.normalize().rescaled_data
     assert norm_data.min()==0
     assert norm_data.max()==1
+
+
+
